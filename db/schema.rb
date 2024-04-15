f7c1a7f67cba49c66b4d0c6e4d69ab2fa01d7c2d
EC2 Default User <ec2-user@ip-172-31-32-108.ap-northeast-1.compute.internal>
2024-04-15 10:53:11 +0000

    migrate修正 references=>integer

--- db/schema.rb
+++ db/schema.rb
@@ -54,21 +54,17 @@ ActiveRecord::Schema.define(version: 2024_04_10_112346) do
 
   create_table "comments", force: :cascade do |t|
     t.text "content"
-    t.integer "user_id", null: false
-    t.integer "post_id", null: false
+    t.integer "user", null: false
+    t.integer "post", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.index ["post_id"], name: "index_comments_on_post_id"
-    t.index ["user_id"], name: "index_comments_on_user_id"
   end
 
   create_table "favorites", force: :cascade do |t|
-    t.integer "user_id", null: false
-    t.integer "post_id", null: false
+    t.integer "user", null: false
+    t.integer "post", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.index ["post_id"], name: "index_favorites_on_post_id"
-    t.index ["user_id"], name: "index_favorites_on_user_id"
   end
 
   create_table "genres", force: :cascade do |t|
@@ -80,12 +76,10 @@ ActiveRecord::Schema.define(version: 2024_04_10_112346) do
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.text "content"
-    t.integer "genre_id"
-    t.integer "user_id", null: false
+    t.integer "user", null: false
+    t.integer "genre", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.index ["genre_id"], name: "index_posts_on_genre_id"
-    t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
   create_table "relationships", force: :cascade do |t|
@@ -111,9 +105,4 @@ ActiveRecord::Schema.define(version: 2024_04_10_112346) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
-  add_foreign_key "comments", "posts"
-  add_foreign_key "comments", "users"
-  add_foreign_key "favorites", "posts"
-  add_foreign_key "favorites", "users"
-  add_foreign_key "posts", "users"
 end
